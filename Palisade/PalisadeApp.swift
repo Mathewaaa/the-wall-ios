7174f5a606d48e39bdfd38e5c1e733ea97b5fab3
Mathewaaa <withdifferentletters@gmail.com>
2022-11-09 17:00:02 -0800

    Initial Commit

---
+++ Palisade/PalisadeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PalisadeApp.swift
+//  Palisade
+//
+//  Created by Mathew Xie on 11/9/22.
+//
+
+import SwiftUI
+
+@main
+struct PalisadeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
