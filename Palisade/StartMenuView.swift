01986ff8587ec8a39a9c24744aaa78aad7d6f13e
Mathewaaa <withdifferentletters@gmail.com>
2022-11-09 17:59:52 -0800

    first commit

---
+++ Palisade/StartMenuView.swift
@@ -0,0 +1,20 @@
+//
+//  StartMenuView.swift
+//  Palisade
+//
+//  Created by Mathew Xie on 11/9/22.
+//
+
+import SwiftUI
+
+struct StartMenuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct StartMenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        StartMenuView()
+    }
+}
